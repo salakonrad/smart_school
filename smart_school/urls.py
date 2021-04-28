8ad2e4803cc862d2ed21e0fde67bb0faa3c71fd6
salakonrad <konradsala98@gmail.com>
2021-04-28 22:07:29 +0200

    fixup! Added Django files

--- smart_school/urls.py
+++ smart_school/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('school.urls'))
+    path('register/', user_views.register, name='register'),
+    path('', include('school.urls')),
 ]
