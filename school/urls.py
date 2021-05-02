bbb7a0ead53eb45520ec2fc49e0940588555a6d9
salakonrad <konradsala98@gmail.com>
2021-05-02 13:25:44 +0200

    fixup! Added Django files

--- school/urls.py
+++ school/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PostListView
 
 urlpatterns = [
     path('', views.home, name='school-home'),
+    # path('', PostListView.as_view(), name='school-home'),
     path('about', views.about, name='school-about'),
 ]
