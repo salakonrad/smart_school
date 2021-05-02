8d87068a5703003fafc133e13d66a3ecee61f53d
salakonrad <konradsala98@gmail.com>
2021-05-02 10:48:39 +0200

    fixup! Added Django files

--- smart_school/settings.py
+++ smart_school/settings.py
@@ -125,8 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'school-home'
