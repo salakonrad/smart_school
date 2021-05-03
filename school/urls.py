0e1c89261b29bb225568b5ec4385bf1079b1557e
salakonrad <konradsala98@gmail.com>
2021-05-03 10:02:26 +0200

    fixup! Added Django files

--- school/urls.py
+++ school/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     # path('', views.home, name='school-home'),
     path('', PostListView.as_view(), name='school-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='school-detail'),
     path('post/create/', PostCreateView.as_view(), name='school-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='school-update'),
